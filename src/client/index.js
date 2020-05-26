99e07436487403a2306aad8d48e1b40e2acc2ecd
Jason Marvin <jdmarvin2002@yahoo.com>
2020-05-25 22:45:37 -0600

    connected js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,9 @@
 import { handleSubmit} from './js/formHandler'
 import { checkForName} from './js/nameChecker'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
 
 console.log(checkForName);
